66806d13d19fce26a1bcbd86fc07a437aaa5836d
Cauan Victor <91569130+CauanDZN@users.noreply.github.com>
2022-11-15 00:06:27 -0300

    :tada: first commit

---
+++ src/App.tsx
@@ -0,0 +1,80 @@
+import {useState} from 'react'
+
+import { Worker } from '@react-pdf-viewer/core';
+import { Viewer } from '@react-pdf-viewer/core';
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+
+function App() {
+
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+
+  const [pdfFile, setPdfFile]=useState(null);
+
+  const [pdfError, setPdfError]=useState('');
+
+
+  const allowedFiles = ['application/pdf'];
+  const handleFile = (e) =>{
+    let selectedFile = e.target.files[0];
+    // console.log(selectedFile.type);
+    if(selectedFile){
+      if(selectedFile&&allowedFiles.includes(selectedFile.type)){
+        let reader = new FileReader();
+        reader.readAsDataURL(selectedFile);
+        reader.onloadend=(e)=>{
+          setPdfError('');
+          setPdfFile(e.target.result);
+        }
+      }
+      else{
+        setPdfError('Por favor, selecione um PDF!');
+        setPdfFile('');
+      }
+    }
+    else{
+      alert('Por favor, selecione um PDF!');
+    }
+  }
+
+  return (
+    <div className="container">
+
+      {/* Upload PDF */}
+      <form>
+
+        <label><h5>Carregue seu PDF</h5></label>
+        <br></br>
+
+        <input type='file' className="form-control"
+        onChange={handleFile}></input>
+
+        {/* we will display error message in case user select some file
+        other than pdf */}
+        {pdfError&&<span className='text-danger'>{pdfError}</span>}
+
+      </form>
+
+      {/* View PDF */}
+      <h5>Visualize seu PDF</h5>
+      <div className="viewer">
+
+        {/* render this if we have a pdf file */}
+        {pdfFile&&(
+          <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.16.105/build/pdf.worker.min.js">
+            <Viewer fileUrl={pdfFile}
+            plugins={[defaultLayoutPluginInstance]}></Viewer>
+          </Worker>
+        )}
+
+        {/* render this if we have pdfFile state null   */}
+        {!pdfFile&&<>No file is selected yet</>}
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
