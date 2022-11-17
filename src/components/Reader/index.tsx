bc71516943a72a42199fc1aad9dda3ae3d025cea
Cauan Victor <91569130+CauanDZN@users.noreply.github.com>
2022-11-16 22:31:35 -0300

    Update index.tsx

--- src/components/Reader/index.tsx
+++ src/components/Reader/index.tsx
@@ -2,8 +2,8 @@ import {useState} from 'react'
 import { Worker } from '@react-pdf-viewer/core';
 import { Viewer } from '@react-pdf-viewer/core';
 import '@react-pdf-viewer/core/lib/styles/index.css';
-import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 import { OnHighlightKeyword } from '@react-pdf-viewer/search';
 
 export function Reader(){
